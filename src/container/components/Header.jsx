702735f8c198eeb643e2a643fc50dc8774f0d3a1
sheriff ibrahim <ibrahimsheriff999@gmail.com>
2023-11-27 09:33:35 +0100

    Added whitepaper Link

--- src/container/components/Header.jsx
+++ src/container/components/Header.jsx
@@ -52,7 +52,7 @@ export default function Header() {
             </div>
             <div className="hidden lg:flex lg:flex-1 lg:justify-end">
                <a
-                  href="#"
+                  href="https://brocktrade.gitbook.io/brocktrade/"
                   className="text-sm font-semibold leading-6 text-gray-900 btn bg-cyan-300 "
                >
                   Read Whitepaper <span aria-hidden="true">&rarr;</span>
@@ -99,7 +99,7 @@ export default function Header() {
                      </div>
                      <div className="py-6">
                         <a
-                           href="#"
+                           href="https://brocktrade.gitbook.io/brocktrade/"
                            className="-mx-3 btn bg-cyan-300  block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                         >
                            Read Whitepaper
